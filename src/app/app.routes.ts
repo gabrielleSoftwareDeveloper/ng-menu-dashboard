ed934150044d92e28d71fdb8030ec06fc2597e2a
gabrielleSoftwareDeveloper <gabriel.orevas96@gmail.com>
2024-09-25 21:51:17 +0200

    First commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,31 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        loadComponent: () => import('./shared/components/layout/layout.component'),
+        children: [
+            {
+                path: 'dashboard',
+                loadComponent: () => import('./business/dashboard/dashboard.component')
+            },
+            {
+                path: 'profile',
+                loadComponent: () => import('./business/profile/profile.component')
+            },
+            {
+                path: 'table',
+                loadComponent: () => import('./business/table/table.component')
+            },
+            {
+                path: '',
+                redirectTo: 'dashboard',
+                pathMatch: 'full'
+            }
+        ]    
+    },
+    { //Control de error si la ruta no existiese.
+        path: '**',
+        redirectTo: 'dashboard'
+    }
+];
